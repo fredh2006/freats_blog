b6db88ffcea601bd1a6dec827737b37c01a17216
Fred He <fhe@bayviewglen.ca>
2024-01-24 20:49:09 -0800

    wingstop

--- freats-blog/src/views/HomeView.vue
+++ freats-blog/src/views/HomeView.vue
@@ -11,19 +11,18 @@ import TheHighlightBanner from '../components/TheHighlightBanner.vue'
       <div class="container left column">
         <h1 class="latest">Latest</h1>
         <div class="metadata-container">
-          <div class="metadata metadata-text">Fred He&nbsp; 2023/1/06</div>
-          <div class="date metadata-text">3 Minute Read</div>
+          <div class="metadata metadata-text">Fred He&nbsp; 2024/1/23</div>
+          <div class="date metadata-text">2 Minute Read</div>
         </div>
-        <img src="../../public/michelin.jpg" class="preview-image" @click="click" />
+        <img src="../../public/wingstop.png" class="preview-image" @click="click" />
       </div>
       <div class="container right column" id="home-text">
-        <h1 class="preview-title">A Guide to the Michelin Guide </h1>
+        <h1 class="preview-title">Review: Wingstop</h1>
         <h1 class="preview-content">
-          No, this article isn’t about that big, white, bubbly, and lowkey creepy Michelin man
-          (sorry, not sorry). Instead, it’s about something else with an odd relationship to this
-          French tire company. The Michelin stars. The widely renowned and recognized term Michelin
-          stars is actually part of the Michelin Guides, a series of guidebooks that Michelin has
-          published and consistently updated since 1900.
+          Wingstop is probably one of the most hyped restaurants on social media in the past few
+          months. So, during the first week back from winter break, my spare group decided to have
+          one extra-long spare and drive downtown to try it. Was it good? Yes. Is it overhyped?
+          Also, yes.
         </h1>
         <div class="button-container">
           <button class="button read-more" @click="click">Read More</button>
@@ -37,7 +36,7 @@ import TheHighlightBanner from '../components/TheHighlightBanner.vue'
 export default {
   methods: {
     click() {
-      this.$router.push('/posts/6599fb56279b11b5f4a9dbc6')
+      this.$router.push('/posts/65b1e7cb279b11b5f4a9ee8a')
     }
   }
 }
