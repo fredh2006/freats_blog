403ff21c888ad8c1127e18bb9750e861e9ba915e
Fred He <fhe@bayviewglen.ca>
2024-02-09 17:43:28 -0800

    article #8

--- freats-blog/src/views/HomeView.vue
+++ freats-blog/src/views/HomeView.vue
@@ -11,18 +11,19 @@ import TheHighlightBanner from '../components/TheHighlightBanner.vue'
       <div class="container left column">
         <h1 class="latest">Latest</h1>
         <div class="metadata-container">
-          <div class="metadata metadata-text">Fred He&nbsp; 2024/1/23</div>
-          <div class="date metadata-text">2 Minute Read</div>
+          <div class="metadata metadata-text">Fred He&nbsp; 2024/2/8</div>
+          <div class="date metadata-text">3 Minute Read</div>
         </div>
-        <img src="../../public/wingstop.png" class="preview-image" @click="click" />
+        <img src="../../public/fries.png" class="preview-image" @click="click" />
       </div>
       <div class="container right column" id="home-text">
-        <h1 class="preview-title">Review: Wingstop</h1>
+        <h1 class="preview-title">The Science Behind Fast Food Fries</h1>
         <h1 class="preview-content">
-          Wingstop is probably one of the most hyped restaurants on social media in the past few
-          months. So, during the first week back from winter break, my spare group decided to have
-          one extra-long spare and drive downtown to try it. Was it good? Yes. Is it overhyped?
-          Also, yes.
+          Why the **** does McDonalds have 19 ingredients in their fries? That was my initial
+          thought when I first learned about it in my very productive, enjoyable Kinesiology class.
+          The idea that French fries could contain more than 2 ingredients appalled me, and I
+          researched it further to find any justification for my love for McDonald’s fries. What I
+          found out was honestly quite interesting.
         </h1>
         <div class="button-container">
           <button class="button read-more" @click="click">Read More</button>
@@ -36,7 +37,7 @@ import TheHighlightBanner from '../components/TheHighlightBanner.vue'
 export default {
   methods: {
     click() {
-      this.$router.push('/posts/65b1e7cb279b11b5f4a9ee8a')
+      this.$router.push('/posts/65c6d3bc3b99dcd2159b49f3')
     }
   }
 }
