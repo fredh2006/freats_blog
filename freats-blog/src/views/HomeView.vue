4fb11848bd38057ca35f524ee65096bdec49202f
Fred He <fhe@bayviewglen.ca>
2024-03-16 19:12:15 -0700

    street food

--- freats-blog/src/views/HomeView.vue
+++ freats-blog/src/views/HomeView.vue
@@ -11,19 +11,21 @@ import TheHighlightBanner from '../components/TheHighlightBanner.vue'
       <div class="container left column">
         <h1 class="latest">Latest</h1>
         <div class="metadata-container">
-          <div class="metadata metadata-text">Fred He&nbsp; 2024/2/8</div>
-          <div class="date metadata-text">3 Minute Read</div>
+          <div class="metadata metadata-text">Fred He&nbsp; 2024/3/16</div>
+          <div class="date metadata-text">2 Minute Read</div>
         </div>
-        <img src="../../public/fries.png" class="preview-image" @click="click" />
+        <img src="../../public/street_food.jpg" class="preview-image" @click="click" />
       </div>
       <div class="container right column" id="home-text">
-        <h1 class="preview-title">The Science Behind Fast Food Fries</h1>
+        <h1 class="preview-title">The Evolution of Street Food</h1>
         <h1 class="preview-content">
-          Why the **** does McDonalds have 19 ingredients in their fries? That was my initial
-          thought when I first learned about it in my very productive, enjoyable Kinesiology class.
-          The idea that French fries could contain more than 2 ingredients appalled me, and I
-          researched it further to find any justification for my love for McDonald’s fries. What I
-          found out was honestly quite interesting.
+          Whether in the bustling alleys of Singapore, the concrete jungle of New York, or the
+          vibrant streets of India, you’ll always see hawker stalls, food carts and street vendors
+          throughout. Street food is a staple of any city and supports the people of the city in
+          various ways. Not only does it provide a meal that’s affordable, available, and filling,
+          but it also gives one the opportunity to taste the cuisine of a country from across the
+          world, often in its realest form. Street food has evolved from a way to feed the working
+          class into a global phenom.
         </h1>
         <div class="button-container">
           <button class="button read-more" @click="click">Read More</button>
@@ -37,7 +39,7 @@ import TheHighlightBanner from '../components/TheHighlightBanner.vue'
 export default {
   methods: {
     click() {
-      this.$router.push('/posts/65c6d3bc3b99dcd2159b49f3')
+      this.$router.push('/posts/65f6509cb772c99cb59322c5')
     }
   }
 }
